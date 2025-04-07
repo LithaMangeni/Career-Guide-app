9c5d278ea363696466fed3c6c2f7140fb3806765
Litha Mangeni <litha.hashe.tlabs@gmail.com>
2025-04-07 10:31:57 +0200

    Added sign-in page

---
+++ Scripts/navbar.js
@@ -0,0 +1,11 @@
+    // // Select the first element with the class 'btn'
+    // const button = document.querySelector('.sign-in-button');
+
+    // // Ensure the button exists before adding the event listener
+    // if (button) {
+    //     button.addEventListener('click', function() {
+    //         window.location.href = 'website.html'; // Replace 'login.html' with your actual login page URL
+    //     });
+    // } else {
+    //     console.error('No element found with the class "btn".');
+    // }
